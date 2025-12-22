342b6b784dc79929fdbc54ea6bdc181070507292
Alice-clr <kahfina15@gmail.com>
2025-12-22 18:57:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 5000); // Gambar berganti setiap 5 detik
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    
+    // Pastikan ada slide sebelum menjalankan
+    if (slides.length === 0) return;
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    
+    slides[slideIndex-1].style.display = "block";  
+    
+    // Berganti setiap 5 detik
+    setTimeout(showSlides, 5000); 
 }
